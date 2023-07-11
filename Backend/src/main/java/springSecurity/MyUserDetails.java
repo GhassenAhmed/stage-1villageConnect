ba2f0b94cfa301b07e3bf460165375e0b07e6959
ghassen <ghassenahmed74@gmail.com>
2023-07-11 18:57:34 +0100

    config MyUserDetails

--- Backend/src/main/java/springSecurity/MyUserDetails.java
+++ Backend/src/main/java/springSecurity/MyUserDetails.java
@@ -37,37 +37,37 @@ public class MyUserDetails implements UserDetails {
 		@Override
 		public String getPassword() {
 			// TODO Auto-generated method stub
-			return null;
+			return user.getPassword();
 		}
 
 		@Override
 		public String getUsername() {
 			// TODO Auto-generated method stub
-			return null;
+			return user.getEmail();
 		}
 
 		@Override
 		public boolean isAccountNonExpired() {
 			// TODO Auto-generated method stub
-			return false;
+			return true;
 		}
 
 		@Override
 		public boolean isAccountNonLocked() {
 			// TODO Auto-generated method stub
-			return false;
+			return true;
 		}
 
 		@Override
 		public boolean isCredentialsNonExpired() {
 			// TODO Auto-generated method stub
-			return false;
+			return true;
 		}
 
 		@Override
 		public boolean isEnabled() {
 			// TODO Auto-generated method stub
-			return false;
+			return true;
 		}
 		
 		
