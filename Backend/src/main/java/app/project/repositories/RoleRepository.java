4b75128c1a6dc2b10bc90585e0ebd2e4764d6c10
ghassen <ghassenahmed74@gmail.com>
2023-07-14 19:00:07 +0100

    add query

--- Backend/src/main/java/app/project/repositories/RoleRepository.java
+++ Backend/src/main/java/app/project/repositories/RoleRepository.java
@@ -1,9 +1,15 @@
 package app.project.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import app.project.entities.Role;
+import app.project.entities.User;
 
 public interface RoleRepository extends JpaRepository<Role, Long>{
 
+	@Query(value="select role_name from Role where id=1 ",nativeQuery=true)
+	Role getRoleClient();
 }
