476c45cd5bc5d94ecb1e8a1e15d46bcd8d1b8e09
ghassen <ghassenahmed74@gmail.com>
2023-07-14 02:07:45 +0100

    create homeView

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="home">
+        <Navbar></Navbar>
+        <main>
+        <Introduction></Introduction>
+        </main>
+        <footer>
+        <Footer></Footer>
+        </footer>
+    </div>
+</template>
+
+<script>
+import Navbar from '../components/home/Navbar.vue';
+import Introduction from '../components/home/Introduction.vue';
+import Footer from '../components/home/Footer.vue';
+export default {
+    name: 'HomeView',
+    components:{
+    Navbar,Introduction,Footer
+    },
+}
+
+</script>
+<style scoped>
+*{
+  box-sizing: border-box;
+  overflow: hidden;
+  scroll-behavior: smooth;
+}
+</style>
