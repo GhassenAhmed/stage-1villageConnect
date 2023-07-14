744765360ecfd90f42b0ee546992159b6703ff9d
ghassen <ghassenahmed74@gmail.com>
2023-07-14 18:05:49 +0100

    create signup component

--- frontend/src/components/authentification/SignUp.vue
+++ frontend/src/components/authentification/SignUp.vue
@@ -1,6 +1,90 @@
 <template>
     <div class="signup">
-        heloo
+        <v-toolbar class="mb-4" >
+            <v-toolbar-title>
+                <img src="../../assets/logo.png" width="200px" alt="logo_app">
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <router-link to="/">
+            <v-btn dense >
+                <v-icon color="#4F5BD8">mdi-home</v-icon>  
+            </v-btn>
+            </router-link>
+        </v-toolbar>
+        <v-container style="background-color: #fff;">
+            <v-layout class="mt-15">
+                <v-flex>
+                   <h1 style="font-size: 25px;margin-bottom: 10px;">Creer un compte</h1>
+                   <p>Accédez à des fonctionnalités exclusives en créant un compte.</p>
+                   
+                   <form @submit.prevent="" enctype="multipart/form-data">
+                        <v-layout  wrap row class="pa-4">
+                                <v-flex class="mt-5 mr-5">
+                                    <v-text-field
+                                            name="Name"
+                                            label="FirstName"
+                                            append-icon="mdi-rename-outline"
+                                            type="text"
+                                            dense
+                                            outlined
+                                            placeholder="Enter FirstName"
+                                    ></v-text-field>
+                                    <v-text-field
+                                            name="Email"
+                                            label="E-mail"
+                                            append-icon="mdi-account"
+                                            type="text"
+                                            outlined
+                                            dense
+                                            placeholder="Enter E-mail"
+                                    ></v-text-field>
+                                    <v-text-field
+                                        name="file"
+                                        id="file"
+                                        label="Your Photo"
+                                        type="file"
+                                    ></v-text-field>
+                                </v-flex>
+
+                                <v-flex class="mt-5 mr-5">
+                                    <v-text-field
+                                            name="Name"
+                                            label="LastName"
+                                            append-icon="mdi-rename-outline"
+                                            type="text"
+                                            dense
+                                            outlined
+                                            placeholder="Enter LastName"
+                                    ></v-text-field>
+                                    <v-text-field
+                                        name="Password"
+                                        label="Password"
+                                        :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+                                        :type="show ? 'text' : 'password'"
+                                        @click:append="show = !show"
+                                        dense
+                                        outlined
+                                        placeholder="Enter Password"
+                                    ></v-text-field>
+                                </v-flex>
+                        </v-layout>
+                        <hr color="#2B3277" size="1px" class="mb-3">
+                        <div class="flex text-center mt-10">
+                            <v-btn :loading="loading" type="submit" color="#4F5BD8" style="color:#fff;font-weight:bold;">
+                                Create my account
+                            </v-btn>
+                        </div>
+                        <br>
+                        <div class="text-center mt-5">
+                            <p>
+                                Vous avez déjà un compte  ? <span style="color:#4F5BD8;font-weight:bold;">
+                                    <router-link to="/Login"> Connectez-vous.</router-link></span> 
+                            </p>
+                        </div>
+                    </form>
+                </v-flex>
+            </v-layout>
+        </v-container>
     </div>
 </template>
 <script>
@@ -10,7 +94,10 @@ export default {
 }
 </script>
 <style scoped>
-.signup{
-    background-color: aqua;
+.container{
+    background-color: #FBFDFE;
+    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1600 900'%3E%3Cdefs%3E%3ClinearGradient id='a' x1='0' x2='0' y1='1' y2='0' gradientTransform='rotate(0,0.5,0.5)'%3E%3Cstop offset='0' stop-color='%23F2FFDA'/%3E%3Cstop offset='1' stop-color='%23CA92C7'/%3E%3C/linearGradient%3E%3ClinearGradient id='b' x1='0' x2='0' y1='0' y2='1' gradientTransform='rotate(0,0.5,0.5)'%3E%3Cstop offset='0' stop-color='%237B88D1'/%3E%3Cstop offset='1' stop-color='%23997BD1'/%3E%3C/linearGradient%3E%3C/defs%3E%3Cg fill='%23FFF' fill-opacity='0' stroke-miterlimit='10'%3E%3Cg stroke='url(%23a)' stroke-width='3.3'%3E%3Cpath d='M1409 581 1450.35 511 1490 581z'/%3E%3Ccircle stroke-width='1.1' transform='' cx='500' cy='100' r='40'/%3E%3Cpath transform='' d='M400.86 735.5h-83.73c0-23.12 18.74-41.87 41.87-41.87S400.86 712.38 400.86 735.5z'/%3E%3C/g%3E%3Cg stroke='url(%23b)' stroke-width='1'%3E%3Cpath transform='' d='M149.8 345.2 118.4 389.8 149.8 434.4 181.2 389.8z'/%3E%3Crect stroke-width='2.2' transform='' x='1039' y='709' width='100' height='100'/%3E%3Cpath transform='' d='M1426.8 132.4 1405.7 168.8 1363.7 168.8 1342.7 132.4 1363.7 96 1405.7 96z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+    background-attachment: fixed;
+    background-size: cover;
 }
 </style>
