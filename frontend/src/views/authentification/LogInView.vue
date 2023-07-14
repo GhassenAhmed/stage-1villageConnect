930a7bd2c50441ca443f60a36fefc880f703eba7
ghassen <ghassenahmed74@gmail.com>
2023-07-14 18:06:18 +0100

    create login view

---
+++ frontend/src/views/authentification/LogInView.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="signup">
+        <LogIn></LogIn>
+    </div>
+</template>
+<script>
+import LogIn from "@/components/authentification/LogIn.vue";
+export default {
+    name:'LogInView',
+    components:{
+        LogIn
+    },
+}
+</script>
+<style scoped>
+.signup{
+    box-sizing: border-box;
+    overflow: hidden;
+    scroll-behavior: smooth;
+    background-color: #FBFDFE;
+    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1600 900'%3E%3Cdefs%3E%3ClinearGradient id='a' x1='0' x2='0' y1='1' y2='0' gradientTransform='rotate(0,0.5,0.5)'%3E%3Cstop offset='0' stop-color='%23F2FFDA'/%3E%3Cstop offset='1' stop-color='%23CA92C7'/%3E%3C/linearGradient%3E%3ClinearGradient id='b' x1='0' x2='0' y1='0' y2='1' gradientTransform='rotate(0,0.5,0.5)'%3E%3Cstop offset='0' stop-color='%237B88D1'/%3E%3Cstop offset='1' stop-color='%23997BD1'/%3E%3C/linearGradient%3E%3C/defs%3E%3Cg fill='%23FFF' fill-opacity='0' stroke-miterlimit='10'%3E%3Cg stroke='url(%23a)' stroke-width='3.3'%3E%3Cpath d='M1409 581 1450.35 511 1490 581z'/%3E%3Ccircle stroke-width='1.1' transform='' cx='500' cy='100' r='40'/%3E%3Cpath transform='' d='M400.86 735.5h-83.73c0-23.12 18.74-41.87 41.87-41.87S400.86 712.38 400.86 735.5z'/%3E%3C/g%3E%3Cg stroke='url(%23b)' stroke-width='1'%3E%3Cpath transform='' d='M149.8 345.2 118.4 389.8 149.8 434.4 181.2 389.8z'/%3E%3Crect stroke-width='2.2' transform='' x='1039' y='709' width='100' height='100'/%3E%3Cpath transform='' d='M1426.8 132.4 1405.7 168.8 1363.7 168.8 1342.7 132.4 1363.7 96 1405.7 96z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+    background-attachment: fixed;
+    background-size: cover;
+}
+
+</style>
