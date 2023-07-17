083dc3c501954ffe3de04235352f2e0d7f210a6d
ghassen <ghassenahmed74@gmail.com>
2023-07-17 02:40:16 +0100

    add testxist email method

--- Backend/src/main/java/app/project/controllers/AuthController.java
+++ Backend/src/main/java/app/project/controllers/AuthController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import app.project.entities.Role;
@@ -117,5 +118,10 @@ public class AuthController {
 		
 	}
 	
+	
+	 @GetMapping("/ExistMail")
+	    public Boolean TestExitEmail(@RequestParam("email") String email) {
+	    	return userService.getByEmail(email)==null ? true :false;
+	    }
 	
 }
