0e828a350e1c192c16e6e64d3431b4814d9ee110
ghassen <ghassenahmed74@gmail.com>
2023-07-17 17:03:40 +0100

    add logIn function in AuthServices

--- frontend/src/services/AuthServices.js
+++ frontend/src/services/AuthServices.js
@@ -11,7 +11,29 @@ export default {
         return Axios.post("verify?email=" + email);
     },
     async login(email, password) {
-        await Axios.post("logIn", { email, password });
+        const store=AuthUser();
+        const response=await Axios.post("logIn", { email, password });
+        if (response.status==200){
+            const roles=response.data.user['roles'];
+            var client=false;
+            var admin=false;
+            var serviceProvider=false;
+
+            for(let i=0;i<roles.length;i++){
+                if(roles[i].roleName=="client"){
+                    client=true;
+                }
+                if(roles[i].roleName=="admin"){
+                    admin=true;
+                }
+                if(roles[i].roleName=="serviceProvider"){
+                    serviceProvider=true;
+                }
+            }
+            store.logIn(response.data.token,response.data.user,admin,client,serviceProvider)
+        }else{
+            store.logOut();
+        }
     },
 
 
