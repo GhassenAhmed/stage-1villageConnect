68ba833c0967033504a04329ebb1a8d33400240d
ghassen <ghassenahmed74@gmail.com>
2023-07-18 02:01:13 +0100

    add service entity with relation -> categorie

---
+++ Backend/src/main/java/app/project/entities/Service.java
@@ -0,0 +1,61 @@
+package app.project.entities;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Service implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long Id;
+	
+	private String serviceName;
+	
+	@ColumnDefault(value="null")
+	private String thumbnailUrl;
+	
+	
+	private  int hiredCount;
+	private boolean isOnline;
+	private int yearsInBusiness;
+	private String pricesRange;
+	
+	@ColumnDefault(value="false")
+	private boolean isBackgroundVerified;
+	private boolean isHomeService;
+
+	
+	@CreationTimestamp
+	private Timestamp created_at;
+	@UpdateTimestamp
+	private Timestamp updated_at;
+	
+	
+	@ManyToOne
+	@JoinColumn(name="service_id")
+	private Service service;
+	
+	
+	
+}
