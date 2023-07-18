add24467de673761dfa4578bedcda001c043eb6d
ghassen <ghassenahmed74@gmail.com>
2023-07-18 02:22:34 +0100

    create schedule entity

---
+++ Backend/src/main/java/app/project/entities/Schedule.java
@@ -0,0 +1,48 @@
+package app.project.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+import org.hibernate.annotations.ColumnDefault;
+
+import lombok.AllArgsConstructor;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Schedule implements Serializable{
+	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long Id;
+	
+	@ColumnDefault(value="true")
+	private boolean isOpenToWork;
+	
+	private String startHour;
+	private String endHour;
+	
+	@OneToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name="service_id",referencedColumnName = "id")
+	private Service service;
+	
+	      
+
+}
