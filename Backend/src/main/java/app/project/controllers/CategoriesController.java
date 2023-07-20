e4d3f98123c16435dec87a43dd9dc5039e850ba2
ghassen <ghassenahmed74@gmail.com>
2023-07-20 04:15:47 +0100

    add pagination

--- Backend/src/main/java/app/project/controllers/CategoriesController.java
+++ Backend/src/main/java/app/project/controllers/CategoriesController.java
@@ -3,11 +3,16 @@ package app.project.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import app.project.entities.Categorie;
+import app.project.parametre.DataCategorie;
 import app.project.repositories.CategorieRepository;
 
 @CrossOrigin(origins = "http://localhost:8081")
@@ -20,8 +25,20 @@ public class CategoriesController {
 	
 	
 	@GetMapping("/getAllCategorie")
-	public List<Categorie> getAllCategories(){
-		return categorieRepository.getAllCategories();
+	public ResponseEntity<?> getAllCategories(@RequestParam(name = "page", defaultValue = "0") int page,
+			@RequestParam(name = "per_page", defaultValue = "5") int size){
+		Page<Categorie> categories;
+		
+		categories=categorieRepository.findAll(PageRequest.of(page, size));
+		
+		int totale = categories.getTotalPages();
+		int[] count_page = new int[totale];
+		for (int i = 0; i < totale; i++) {
+			count_page[i] = i;
+		}
+		DataCategorie data=new DataCategorie(count_page,categories,page);
+		
+		return ResponseEntity.ok(data);
 	}
 
 }
