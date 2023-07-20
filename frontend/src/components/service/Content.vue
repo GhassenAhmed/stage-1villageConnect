ea76595ecd43d4f798524840feb42aa881d0bfe5
ghassen <ghassenahmed74@gmail.com>
2023-07-20 08:41:20 +0100

    fix design

--- frontend/src/components/service/Content.vue
+++ frontend/src/components/service/Content.vue
@@ -9,7 +9,7 @@
                         mdi-chevron-left
                     </v-icon>
                 </v-btn> 
-                <li v-for="categorie in categories" :key="categorie.id" style="list-style-type: none; margin: 0px 0px 20px ;">{{categorie.categorieName}}</li>
+                <li v-for="categorie in categories" :key="categorie.id" style="list-style-type: none; margin: 0px 0px 20px ;font-size: 20px;">{{categorie.categorieName}}</li>
                 <v-btn small plain :disabled="testNext==true"  @click="changerPage(pageCurrent+1)" style="margin: 0px 0px 20px ;">
                     <v-icon>
                         mdi-chevron-right
@@ -18,17 +18,17 @@
             </div>
         </v-layout> 
         <v-container style="margin-top: 50px;">
-            <p style="font-size: 40px;">Hello!Ghassen</p>
-            <div class="introduction" style="border: 0.1mm solid #CFD0D3; height: auto;border-radius: 10px;padding: 20px 25px 20px 25px;">
-                <v-layout  row wrap>
+            <p style="font-size: 30px;padding-left:  70px;font-weight: bold;">Salut,ghassen</p>
+            <div class="introduction" style="border: 0.1mm solid #CFD0D3; height: auto;border-radius: 10px;padding: 30px 25px 20px 30px;">
+                <v-layout  row wrap class="">
                     <v-flex>
                         <v-layout row wrap>
                             <v-flex class="d-flex justify-center align-center"> 
-                                    <img src="../../assets/catalogue.png" alt="" width="100px" >
+                                    <img src="../../assets/smile.png" alt="" width="70px" class="hidden-sm-and-down">
                             </v-flex>
                             <v-flex class="pa-5 mt-xs-0">
-                                <p style="font-weight: bold;margin-bottom: 5px;" class="">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-                                <p class="">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                                <p style="font-weight: bold;margin-bottom: 5px;font-size: 20px;"> Découvrez les services de votre village.</p>
+                                <p style="font-weight: normal;font-size: 18px;">Ajoutez vos services et créez votre propre portfolio.</p>
                             </v-flex>
                         </v-layout>  
                     </v-flex>
@@ -37,17 +37,19 @@
             </div>
 
             <!-- ----------------------------------------Most raited div------------------------------  -->
-            <div class="mostRaited" style="height: auto;border: 0.1mm solid #CFD0D3;border-radius: 10px;margin-top: 50px;">
-                <p style="font-size: 30px;padding-left:  70px;padding-top: 20px;">Most raited !</p>
+            <div class="mostRaited" style="height: auto;border: 0.1mm solid #CFD0D3;border-radius: 10px;margin-top: 100px;">
+                <div class="div d-flex" style="padding-left:  70px;padding-top: 20px;padding-bottom: 20px;">
+                    <img src="../../assets/star.png" alt="" width="70px" style="margin-right:20px;" class="hidden-sm-and-down">
+                    <p style="font-size: 30px;padding-top: 20px;">Le plus évalué ! </p>
+                </div>
                 <div class="card d-flex wrap">
                     <v-card
-                    
                     class="mx-auto my-5"
                     max-width="250"
                     min-width="100"
                     v-for="i in 3"
                     :key="i"
-                >
+                   >
                     <template slot="progress">
                     <v-progress-linear
                         color="deep-purple"
@@ -166,9 +168,20 @@ export default {
 </script>
 <style scoped>
 *{
-    font-family: Tahoma, Geneva, sans-serif;
+    font-family:cursive;
     scroll-behavior: smooth;
     overflow: hidden;
     box-sizing: border-box;
+}
+
+li{
+    display: block;
+    padding-bottom: 10px;
+    padding-top: 10px;
+    border-bottom: 3.5px solid #ffffff;
+}
+li:hover{
+    border-bottom: 3.5px solid #12c2b9;
+    overflow: hidden;
 }
 </style>
