c9a7185c8331319e38578d0b41a63fc26570c634
ghassen <ghassenahmed74@gmail.com>
2023-07-20 04:16:02 +0100

    add dataCategorie Class

---
+++ Backend/src/main/java/app/project/parametre/DataCategorie.java
@@ -0,0 +1,17 @@
+package app.project.parametre;
+
+import org.springframework.data.domain.Page;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DataCategorie {
+
+	private int[] count_page;
+	private Page<app.project.entities.Categorie> Categorie;
+	int page;
+}
