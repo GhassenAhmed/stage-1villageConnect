17fb95940a911744367ef8566bd50be77595fd79
ghassen <ghassenahmed74@gmail.com>
2023-07-21 01:31:43 +0100

    create villageController + fuunction getAllVillages

---
+++ Backend/src/main/java/app/project/controllers/VillageController.java
@@ -0,0 +1,26 @@
+package app.project.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import app.project.entities.Village;
+import app.project.repositories.VillageRepository;
+
+@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+public class VillageController {
+	
+	@Autowired
+	VillageRepository villageRepository;
+	
+	
+	@GetMapping("/getAllVillages")
+	public List<Village> getAllVillages(){
+		return villageRepository.findAll();
+	}
+
+}
