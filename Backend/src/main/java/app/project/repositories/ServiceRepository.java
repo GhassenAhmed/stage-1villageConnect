5a13424f9ab7b7d84e1c507f003318ffa1ca5741
ghassen <ghassenahmed74@gmail.com>
2023-07-22 04:52:50 +0100

    add query

--- Backend/src/main/java/app/project/repositories/ServiceRepository.java
+++ Backend/src/main/java/app/project/repositories/ServiceRepository.java
@@ -1,12 +1,18 @@
 package app.project.repositories;
 
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.http.ResponseEntity;
 
 import app.project.entities.Service;
+import app.project.entities.User;
 
 
 
@@ -14,7 +20,8 @@ import app.project.entities.Service;
 @EnableJpaRepositories
 public interface ServiceRepository  extends JpaRepository<Service, Long>{
 
-	
+	@Query(value="select * from service where categorie_id=:id",nativeQuery=true)
+	List<Service> getServiceCategorie(Long id);
 	
 
 	
