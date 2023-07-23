6303457b295a665c17af4c735f0893f16f7e98e5
ghassen <ghassenahmed74@gmail.com>
2023-07-23 19:13:33 +0100

    add function

--- Backend/src/main/java/app/project/repositories/VillageRepository.java
+++ Backend/src/main/java/app/project/repositories/VillageRepository.java
@@ -2,15 +2,18 @@ package app.project.repositories;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import app.project.entities.Service;
 import app.project.entities.Village;
 
 
 public interface VillageRepository extends JpaRepository<Village, Long> {
 
 	List<Village> findAll();
-	
 	
 
 		
