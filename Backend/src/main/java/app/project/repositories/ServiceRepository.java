a28dd9d291f94af219e0a93decfe120401cfadba
ghassen <ghassenahmed74@gmail.com>
2023-07-23 19:13:24 +0100

    add qyery

--- Backend/src/main/java/app/project/repositories/ServiceRepository.java
+++ Backend/src/main/java/app/project/repositories/ServiceRepository.java
@@ -2,26 +2,33 @@ package app.project.repositories;
 
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.http.ResponseEntity;
-
 import app.project.entities.Service;
-import app.project.entities.User;
 
 
 
 
-@EnableJpaRepositories
+
+
 public interface ServiceRepository  extends JpaRepository<Service, Long>{
 
 	@Query(value="select * from service where categorie_id=:id",nativeQuery=true)
 	List<Service> getServiceCategorie(Long id);
+	
+	@Query(value="select * from service order by raiting desc",nativeQuery=true)
+	Page<Service> getServiceRaiting(Pageable pageable );
+	
+	@Query(value="select * from service s,village v where s.id=v.id and v.village_name LIKE ? order by s.raiting desc",nativeQuery=true)
+	Page<Service> getServiceRaitingByVillage(String village,Pageable pageable);
+	
+	
+	
+	
 	
 
 	
