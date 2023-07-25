ab88a6ad665df6b089f463756bd5fbdab24efbd5
ghassen <ghassenahmed74@gmail.com>
2023-07-25 12:26:06 +0100

    fix function

--- frontend/src/services/ServiceServices.js
+++ frontend/src/services/ServiceServices.js
@@ -3,8 +3,8 @@ import Axios from "axios";
 import "@/plugins/axios"
 
 export default {
-    async getServiceRaiting(page,search,per){
-        const res= await Axios.get(`getServiceRaiting?${search ? "search="+search : ""}&page=${page}&per_page=${per}`);
+    async getServiceRaiting(page,id,per){
+        const res= await Axios.get(`getServiceRaiting?id=${id}&page=${page}&per_page=${per}`);
         return res;
     },
 }
