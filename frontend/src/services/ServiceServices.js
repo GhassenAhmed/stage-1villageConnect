610564c37573685a99ab13b47bae1b01527da84e
ghassen <ghassenahmed74@gmail.com>
2023-07-25 14:13:12 +0100

    add method get serviceVerified

--- frontend/src/services/ServiceServices.js
+++ frontend/src/services/ServiceServices.js
@@ -7,4 +7,8 @@ export default {
         const res= await Axios.get(`getServiceRaiting?id=${id}&page=${page}&per_page=${per}`);
         return res;
     },
+    async getServiceVerified(page,id,per){
+        const res= await Axios.get(`getServiceVerified?id=${id}&page=${page}&per_page=${per}`);
+        return res;
+    },
 }
