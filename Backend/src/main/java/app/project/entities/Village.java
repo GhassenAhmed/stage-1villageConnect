1af8e26c15b498e788fbaf0f0042bc4f836ef466
ghassen <ghassenahmed74@gmail.com>
2023-07-30 15:30:27 +0100

    fix entity

--- Backend/src/main/java/app/project/entities/Village.java
+++ Backend/src/main/java/app/project/entities/Village.java
@@ -2,16 +2,12 @@ package app.project.entities;
 
 import java.io.Serializable;
 import java.sql.Timestamp;
-import java.util.Set;
+
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
