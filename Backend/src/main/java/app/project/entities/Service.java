920bc99128118513fb2f40baa27017eb7a8cfad8
ghassen <ghassenahmed74@gmail.com>
2023-07-30 15:30:15 +0100

    fix entity

--- Backend/src/main/java/app/project/entities/Service.java
+++ Backend/src/main/java/app/project/entities/Service.java
@@ -10,7 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
+
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
