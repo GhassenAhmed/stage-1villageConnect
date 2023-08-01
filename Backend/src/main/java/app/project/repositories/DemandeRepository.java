0fd21a77f71bc5c301dd696adf3f713252269449
ghassen <ghassenahmed74@gmail.com>
2023-08-01 15:40:26 +0100

    delete repository

--- Backend/src/main/java/app/project/repositories/DemandeRepository.java
+++
@@ -1,9 +0,0 @@
-package app.project.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import app.project.entities.Demande;
-
-public interface DemandeRepository extends JpaRepository<Demande, Long>{
-
-}
