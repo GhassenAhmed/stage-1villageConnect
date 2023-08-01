71c1e8deab4d80c91df0cbe7c67bba732dc4c5f6
ghassen <ghassenahmed74@gmail.com>
2023-08-01 15:41:49 +0100

    add service view

---
+++ frontend/src/views/interface/admin/ServicesView.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="root">
+       <Navbar></Navbar>
+       hhhhh
+    </div>
+  </template>
+  <script>
+  import { AuthUser } from "@/store/AuthStore";
+  import Navbar from '@/components/admin/Navbar.vue'
+    export default {
+      components:{
+        Navbar
+     },
+      setup(){
+        const store=AuthUser();
+        return{
+            store
+        }
+    },
+    created(){
+       
+    },
+    name:'ServicesView',
+data(){
+    return{
+        drawer: false,
+        mini: true,
+        
+    }
+},
+methods:{
+    
+},
+  computed:{
+     
+  }
+}
+  </script>
