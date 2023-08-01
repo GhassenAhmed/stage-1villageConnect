eb0fe7bfe82526d9f79b69ede9a57a8e5a3341c8
ghassen <ghassenahmed74@gmail.com>
2023-08-01 15:42:03 +0100

    add user view

---
+++ frontend/src/views/interface/admin/UsersView.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="root">
+       <Navbar></Navbar>
+       user
+    </div>
+  </template>
+  <script>
+  import { AuthUser } from "@/store/AuthStore";
+  import Navbar from '@/components/admin/Navbar.vue'
+    export default {
+      components:{
+        Navbar
+     },
+      setup(){
+        const store=AuthUser();
+        return{
+            store
+        }
+    },
+    created(){
+       
+    },
+    name:'UsersView',
+data(){
+    return{
+        drawer: false,
+        mini: true,
+        
+    }
+},
+methods:{
+    
+},
+  computed:{
+     
+  }
+}
+  </script>
