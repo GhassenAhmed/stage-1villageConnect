a39e8af59ae47f76f825515910cbd4c479077824
ghassen <ghassenahmed74@gmail.com>
2023-08-08 19:15:59 +0100

    addd class data user

---
+++ Backend/src/main/java/app/project/parametre/DataUser.java
@@ -0,0 +1,11 @@
+package app.project.parametre;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data@NoArgsConstructor@AllArgsConstructor
+public class DataUser {
+	private String FirstName;
+	private String LastName;
+}
