f4b2b9a71c2a95397eb8923a0a0a355b9c343c9b
ghassen <ghassenahmed74@gmail.com>
2023-08-08 19:15:37 +0100

    add methods editprofil + edit photo

--- Backend/src/main/java/app/project/controllers/UserController.java
+++ Backend/src/main/java/app/project/controllers/UserController.java
@@ -2,13 +2,19 @@ package app.project.controllers;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import app.project.entities.User;
+import app.project.parametre.DataPhoto;
+import app.project.parametre.DataUser;
 import app.project.repositories.UserRepository;
 import app.project.services.UserService;
 
@@ -43,6 +49,23 @@ public class UserController {
 	  			return  userRepository.countUsers();	
 	  	}
 	  
+	  
+	  @PostMapping("/uploadPhoto")
+	    public ResponseEntity<?> EditPhoto(@RequestBody DataPhoto photo,HttpServletRequest request){
+	    	  User userAuth=userService.UserAuth(request);
+	    	  userAuth.setPhoto(photo.getPhoto());
+	    	  userRepository.save(userAuth);
+	    	  return  ResponseEntity.ok().body("Photo Upload");
+	    }
+	  
+	  @PostMapping("/EditInfoPersonnel")
+	    public ResponseEntity<?> EditInfoPersonnel(@RequestBody DataUser data,HttpServletRequest request){
+	    	  User userAuth=userService.UserAuth(request);
+	    	  userAuth.setFirstName(data.getFirstName());
+	    	  userAuth.setLastName(data.getLastName());
+	    	  userRepository.save(userAuth);
+	    	  return  ResponseEntity.ok().body("User Modified");
+	    }
 	
 
 }
