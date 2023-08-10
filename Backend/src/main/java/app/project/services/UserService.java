4cae62775b22e86c7d4bcbeee47749715d4268a2
ghassen <ghassenahmed74@gmail.com>
2023-08-10 15:30:26 +0100

    fix injection

--- Backend/src/main/java/app/project/services/UserService.java
+++ Backend/src/main/java/app/project/services/UserService.java
@@ -1,12 +1,16 @@
 package app.project.services;
 
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import app.project.Mail.Mail;
 import app.project.entities.User;
 import app.project.repositories.UserRepository;
+import net.bytebuddy.utility.RandomString;
 
 @Service
 public class UserService {
@@ -16,6 +20,9 @@ public class UserService {
 	
 	 @Autowired
 	private app.project.jwt.jwtTokenUtil  jwtTokenUtil;
+	 
+	 @Autowired
+	 Mail mailsender;
 	
 	public User getByEmail(String email) {
 		 try {
@@ -43,6 +50,53 @@ public class UserService {
 	  
 	  public int getUserServiceProvider(){
 		  return userRepository.CountUserServiceProvider();
+	  }
+	  
+	  public void VerifyEmail(String email) throws Exception {
+		  User user=userRepository.getUserByEmail2(email);
+		  if(user==null) {
+			  throw new Exception("User Not Found");
+		  }else {
+			  if(user.getEmail_verified_at()!=null) {
+				  throw new Exception("User Already Verified");
+			  }else {
+				  user.setEmail_verified_at(new Date());
+				  userRepository.save(user);
+			  }
+		  }
+	  }
+	  
+	  public void forgotPassword(String email)throws Exception {
+		  User user=userRepository.getUserByEmail2(email);
+		  if(user==null) {
+			  throw new Exception("Email Not Found");
+		  }else {
+			  String Token=RandomString.make(8);
+			  user.setPassword_token(Token);
+			  user.setPassword_token_send_ats(new Date());
+			  userRepository.save(user);
+			  mailsender.SendForgotPassword(user, Token);
+		   }
+	  }
+	  
+	  public void ChangerPassword(String email,String Token,String password)throws Exception {
+		  User user=userRepository.getUserByEmail2(email);
+		  if(user==null) {
+			  throw new Exception("Email Not Found");
+		  }else {
+			  if(userRepository.CheckToken(Token)==null) {
+				  throw new Exception("Token Not Found");
+			  }else {
+				  if(user.getPassword_token().equals(Token)) {
+					  user.setPassword(password);
+					  user.setPassword_token(null);
+					  user.setPassword_token_send_ats(null);
+					  userRepository.save(user);
+				  }else {
+					  throw new Exception("Token doesn't match");
+				  }
+			  }
+		   }
 	  }
 	
 }
