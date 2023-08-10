c8f6c88335d2899c83eca1dd70bcf188173b3e39
ghassen <ghassenahmed74@gmail.com>
2023-08-10 15:29:46 +0100

    add senforgotpassword

--- Backend/src/main/java/app/project/Mail/Mail.java
+++ Backend/src/main/java/app/project/Mail/Mail.java
@@ -33,7 +33,7 @@ public class Mail {
 		context.setVariable("link","http://localhost:8081/logIn?email="+user.getEmail());
 		String body=templateEngine.process("VerifyEmail", context);
 		
-		String fromAddress = "myeduconnect20@gmail.com";
+		String fromAddress = "villageconnectt@gmail.com";
 		String senderName = "VillageConnect";
          
 		MimeMessage message = javaMailSender.createMimeMessage();
@@ -43,6 +43,52 @@ public class Mail {
 		helper.setFrom(fromAddress, senderName);
 		helper.setTo(toAddress);
 		helper.setSubject("Email address Verification");
+
+		helper.setText(body, true);
+
+		javaMailSender.send(message);
+	}
+	
+	public void SendForgotPassword(User user,String Token)
+			throws MessagingException, UnsupportedEncodingException {
+
+		Context context=new Context();
+		context.setVariable("token", Token);
+		context.setVariable("link","http://localhost:8081/#/changerpassword");
+		String body=templateEngine.process("ForgotPassword", context);
+		
+		String fromAddress = "villageconnectt@gmail.com";
+		String senderName = "VillageConnect";
+         
+		MimeMessage message = javaMailSender.createMimeMessage();
+		
+		MimeMessageHelper helper = new MimeMessageHelper(message,true);
+		String toAddress = user.getEmail();
+		helper.setFrom(fromAddress, senderName);
+		helper.setTo(toAddress);
+		helper.setSubject("Email Forgot Password");
+
+		helper.setText(body, true);
+
+		javaMailSender.send(message);
+	}
+	
+	public void SendMailConfirm(String email_current,String new_email) throws MessagingException ,UnsupportedEncodingException{
+		Context context=new Context();
+		context.setVariable("email_new", new_email);
+		context.setVariable("link","http://localhost:8081/login?email="+email_current+"&email_new="+new_email);
+		String body=templateEngine.process("ConfirmChangeEmail", context);
+		
+		String fromAddress = "villageconnectt@gmail.com";
+		String senderName = "VillageConnect";
+         
+		MimeMessage message = javaMailSender.createMimeMessage();
+		
+		MimeMessageHelper helper = new MimeMessageHelper(message,true);
+		String toAddress = email_current;
+		helper.setFrom(fromAddress, senderName);
+		helper.setTo(toAddress);
+		helper.setSubject("Confirm Change Email");
 
 		helper.setText(body, true);
 
