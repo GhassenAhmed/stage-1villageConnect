15b240b81ded3559d5463fd3465e8d0fcb97add9
ghassen <ghassenahmed74@gmail.com>
2023-08-29 03:39:24 +0100

    aa

--- Backend/src/main/java/app/project/repositories/InboxRepository.java
+++ Backend/src/main/java/app/project/repositories/InboxRepository.java
@@ -1,11 +1,14 @@
 package app.project.repositories;
 
+import java.sql.ResultSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import app.project.entities.Inbox;
+import app.project.entities.User;
 
 public interface InboxRepository extends JpaRepository<Inbox, Long> {
 	 @Query(value="select * from inbox where user_envoi=:envoi and user_recu=:recu or user_envoi=:recu and user_recu=:envoi",nativeQuery=true)
