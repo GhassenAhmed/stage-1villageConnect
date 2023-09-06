6c3c5449c9ee1d86186c04a7691e226136952129
ghassen <ghassenahmed74@gmail.com>
2023-09-06 11:42:20 +0100

    fix bugs

--- frontend/src/views/EditProfilView.vue
+++ frontend/src/views/EditProfilView.vue
@@ -170,6 +170,19 @@
                         </v-list-item-action>
                         
                     </v-list-item>
+                    <v-list-item v-if="store.IsServiceProvider==true">
+                        
+                        <v-list-item-action>
+                            <v-btn
+                                plain
+                               to="/services"
+                            >
+                                <v-icon class="pa-2">mdi-cards-variant</v-icon>
+                                <span class="">Services</span>
+                            </v-btn>
+                        </v-list-item-action>
+                        
+                    </v-list-item>
                     <v-list-item >
                         <v-list-item-action>
                             <v-btn
@@ -231,7 +244,13 @@
 
                     <v-list-item-title style="font-size: 15px;margin-top: 5px;padding-bottom: 5px;">Listes</v-list-item-title>
                 </v-list-item>
+                <v-list-item v-if="store.IsServiceProvider==true" to="/services">   
+                    <v-list-item-icon>
+                    <v-icon X Small>mdi-cards-variant</v-icon>
+                    </v-list-item-icon>
 
+                    <v-list-item-title style="font-size: 15px;margin-top: 5px;padding-bottom: 5px;">Services</v-list-item-title>
+                </v-list-item>
                 <v-list-group
                 no-action
                 sub-group
