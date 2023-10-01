ec256e1d42b21098ee73a613d36eee8f34d0a301
ghassen <ghassenahmed74@gmail.com>
2023-10-01 17:37:54 +0100

    add queries

--- Backend/src/main/java/app/project/repositories/UserRoleRepository.java
+++ Backend/src/main/java/app/project/repositories/UserRoleRepository.java
@@ -1,9 +1,20 @@
 package app.project.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import app.project.entities.UserRole;
 
 public interface UserRoleRepository extends JpaRepository<UserRole, Long>{
-
+	
+	@Query(value="select * from user_role where user_id=:id and role_id=1 and status=0",nativeQuery=true)
+	UserRole getUserRole(long id);
+	
+	@Query(value="select * from user_role where user_id=:id",nativeQuery=true)
+	List<UserRole> getUserRoles(long id);
+	
+	@Query(value="delete from user_role where user_id=:id",nativeQuery=true)
+	UserRole deleteUserRole(long id);
 }
